70621541e6fcb3b24f6263e0063f77b65b37429c
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-10-26 18:39:18 +0300

    added images to the website

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import "./index.scss"
 import LogoS from "../../assets/images/logo-s.png"
+import LogoSubtitle from "../../assets/images/logo_sub.png"
 
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className="logo" to="/">
             <img src={LogoS} alt="logo" />
-            <img src={LogoS} alt="logo" />
+            <img src={LogoSubtitle} alt="patrik" />
         </Link>
     </div>
 )
