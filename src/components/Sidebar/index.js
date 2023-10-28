7f94549a9a5edc163c114af9b2ac7aaf487b7350
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-10-28 11:47:58 +0300

    added basic layout and styling

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import "./index.scss"
 import LogoS from "../../assets/images/logo-s.png"
 import LogoSubtitle from "../../assets/images/logo_sub.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser,faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faHome, faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faLinkedin, faGithub, faSkype } from "@fortawesome/free-brands-svg-icons"
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -13,15 +14,41 @@ const Sidebar = () => (
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
+
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" className="contact-page" />
+
+            <NavLink exact="true" activeclassname="active" className="contact-link"to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" className="contact-page" />
             </NavLink>
-        </nav>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" 
+                    rel="noreferrer" 
+                    href="https://www.linkedin.com/in/patrik-ku%C5%A1n%C3%ADr-6abb1a109/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" 
+                    rel="noreferrer" 
+                    href="https://github.com/patrikkusnir1">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" 
+                    rel="noreferrer" 
+                    href="skype:live:metalangel8">
+                    <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+        
     </div>
 )
 
