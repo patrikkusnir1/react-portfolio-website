081c52c19af20ad8a956c8a4deb65de637d23404
patrikkusnir1 <patrikkusnir1@gmail.com>
2023-10-29 13:43:25 +0300

    added animation for letters in home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,36 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import LogoTitle from "../../assets/images/patrik.png";
+import  AnimatedLetters from "../AnimatedLetters"
+
 import "./index.scss"
 
 
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState("text-animate")
+    const nameArray = ["a", "t", "r", "i", "k"];
+    const jobArray = ["w", "e", "b", "d", "e", "v", "e", "l", "o", "p", "e", "r", "."]
+
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _13`}>i,</span>
+                    <br />
+                    <span className={letterClass}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
-                atrik
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray} 
+                idx={15}/>
                 <br />
-                web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray} 
+                idx={22}/>
                 </h1>
                 <h2>Frontend developer / Pharmacist / Scientist</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
