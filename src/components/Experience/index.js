782f788d0a7bdf7e3e82e684d93fa73607852a19
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2026-02-11 00:24:46 +0300

    add data portfolio

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss"
 
 const Experience = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
   return (
     <>
       <div className="container experience-page">
         <h1 className="page-title">
-          <AnimatedLetters 
+          <AnimatedLetters
+              letterClass={letterClass}
+              strArray={"Experience".split("")}
               idx={15}/>
         </h1>
+        {/* render experience content */}
+        <div>{renderPortfolio()}</div>
       </div>
       <Loader type="pacman" />
     </>
