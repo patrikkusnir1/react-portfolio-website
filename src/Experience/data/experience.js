b11be67e873515ac6f85cb9fded73b322da10dd1
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2026-02-11 12:36:33 +0300

    add portfolio webpage

---
+++ src/Experience/data/experience.js
@@ -0,0 +1,45 @@
+import krka from "../../assets/images/krka.png";
+import jti from "../../assets/images/jti.png";
+import synthesit from "../../assets/images/synthesit.jpg"
+import jlfuk from "../../assets/images/jlfuk.png"
+
+const experience = [
+  {
+    cover: krka,
+    name: 'KRKA-RUS',
+    title: 'Quality Assessment specialist',
+    description:
+      'I currently work as a Quality Assurance Specialist at the Russian branch of the pharmaceutical company KRKA.\nI am responsible for change control quality system across the company, validation and qualification of laboratory instruments and manufacturing equipment.\nI also check and confirm verification of microbiological methods protocols and deviations in climatic parameters.',
+    duration: 'Oct 2024 - Present',
+    url: 'https://www.krka.ru/',
+  },
+  {
+    cover: synthesit,
+    name: 'Scientific Research Center Synthesit, Sochi, Russia',
+    title: 'Medical Research Scientist',
+    description:
+      'I wrote scientific articles about iron citrate supplements in English and published them in European scientific journals. I provided basic patient consultation and advisory. I translated articles from English to Russian and vice versa. Also I supported my non-medical colleages in my professional expertise.',
+    duration: 'Oct 2023 - Jan 2024',
+    url: 'https://synthesit-world.com/',
+  },
+  {
+    cover: jti,
+    name: 'JTI Poland, Warsaw, Poland',
+    title: 'Toxicology Information Manager',
+    description:
+      'I searched scientific databases and literature to collect toxicological, classification, and labeling information related to components used/to be used in JTI products. I read scientific articles, documentation, reports, etc., and assessed the relevance of published toxicological information. I prepared and continuously updated toxicological documentation on ingredient toxicity for tobacco products. I supporting annual classification and labeling of ingredients for our portfolio to support successful chemical registration.',
+    duration: 'June 2021 - Oct 2022',
+    url: 'https://www.jti.com/en',
+  },
+  {
+    cover: jlfuk,
+    name: 'Jessenius Faculty of Medicine, Comenius University',
+    title: 'PhD Student, Department of Pharmacology',
+    description:
+      'During my PhD studies, I was involved in teaching students of general medicine, dentistry, nursing, midwifery, and public health. I also wrote professional articles on tuberculosis and inflammatory markers, which was the topic of my research.',
+    duration: 'Sep 2017 - Mar 2021',
+    url: 'https://www.jfmed.uniba.sk/en/',
+  },
+]
+
+export default experience
--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,10 +1,56 @@
 import React, { useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss"
+import "./index.scss";
+import experience from "../../Experience/data/experience"
+import { useEffect } from "react";
+
 
 const Experience = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
+  console.log(experience);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover")
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  });
+
+  const renderExperience = (experience) => {
+    return (
+      <div className="experience-container">
+        {
+          experience.map((exp, idx) => {
+            return (
+              <div className="experience-card" key={idx}>
+                <div className="logo-wrapper">
+                  <img
+                    src={exp.cover}
+                    className="experience-image"
+                    alt={exp.name}
+                  />
+                </div>
+                <div className="experience-content">
+                  <p className="experience-title">{exp.title}</p>
+                  <p className="experience-company">{exp.name}</p>
+                  <p className="experience-duration">{exp.duration}</p>
+                  <h4 className="experience-description">{exp.description}</h4>
+                  <button className="btn" onClick={() => window.open(exp.url)}>
+                    View company website
+                  </button>
+                </div>
+              </div>
+            )
+          })
+        }
+      
+      </div>
+    );
+  }
   return (
     <>
       <div className="container experience-page">
@@ -15,7 +61,7 @@ const Experience = () => {
               idx={15}/>
         </h1>
         {/* render experience content */}
-        <div>{renderPortfolio()}</div>
+        <div>{renderExperience(experience)}</div>
       </div>
       <Loader type="pacman" />
     </>
