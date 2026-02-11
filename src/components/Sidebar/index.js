c51e1ff68f09e045c46f7aef3ad2aad2e4b1d6e8
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2026-02-11 12:39:54 +0300

    change skype to telegram contact

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,52 +6,72 @@ import { faUser, faHome, faEnvelope, faSuitcase } from "@fortawesome/free-solid-
 import { faLinkedin, faGithub, faSkype } from "@fortawesome/free-brands-svg-icons"
 
 const Sidebar = () => (
-    <div className="nav-bar">
-        <Link className="logo" to="/">
-            <img src={LogoS} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="experience-link" to="/experience">
-                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-            </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="experience-link"
+        to="/experience"
+      >
+        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+      </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" className="contact-page" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank"
-                    rel="noreferrer"
-                    href="https://www.linkedin.com/in/patrik-ku%C5%A1n%C3%ADr-6abb1a109/">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank"
-                    rel="noreferrer"
-                    href="https://github.com/patrikkusnir1">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank"
-                    rel="noreferrer"
-                    href="skype:live:metalangel8">
-                    <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-
-    </div>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          color="#4d4d4e"
+          className="contact-page"
+        />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/patrik-ku%C5%A1n%C3%ADr-6abb1a109/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/patrikkusnir1"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://t.me/patrik523">
+          <FontAwesomeIcon icon={faTelegram} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
 export default Sidebar
